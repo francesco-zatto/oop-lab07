fd3b308dc4f1180ba0d90eb3037ab31f7cb727e9
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2022-11-08 21:53:27 +0100

    Wrote fromString method inside enum Month

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,6 +1,7 @@
 package it.unibo.nestedenum;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -10,19 +11,33 @@ import java.util.Objects;
 public final class MonthSorterNested implements MonthSorter {
 
     enum Month {
-        JANUARY(31), FEBRUARY(28), MARCH(31), APRIL(30),
-        MAY(31), JUNE(30), JULY(31), AUGUST(31),
-        SEPTEMBER(30), OCTOBER(31), NOVEMBER(30), DECEMBER(31);
+        JANUARY(31, "JANUARY"), FEBRUARY(28, "FEBRUARY"), MARCH(31, "MARCH"), 
+        APRIL(30, "APRIL"), MAY(31, "MAY"), JUNE(30, "JUNE"), 
+        JULY(31, "JULY"), AUGUST(31, "AUGUST"), SEPTEMBER(30, "SEPTEMBER"),
+        OCTOBER(31, "OCTOBER"), NOVEMBER(30, "NOVEMBER"), DECEMBER(31, "DECEMBER");
 
         private final int days;
+        private final String name;
 
-        private Month(int days) {
+        private Month(int days, String name) {
             this.days = days;
+            this.name = name;
         }
 
         public Month fromString(String string) {
-            return Month.this;
+            for (Month month : Month.values()) {
+                if (isStringAMonth(string.toUpperCase(), month)) {
+                    return month;
+                }
+            }
+            throw new IllegalArgumentException("Month not found!");
         }
+
+        private static boolean isStringAMonth(String s, Month m) {
+            List<String> ambiguousStrings = List.of("MA", "M", "JU", "J", "A");
+            return !ambiguousStrings.contains(s) && m.name.startsWith(s); 
+        }
+
     }
 
     @Override
