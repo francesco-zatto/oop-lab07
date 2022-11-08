5f54adf2010c5df42b5e8194a9301043b5bbf17e
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2022-11-08 20:08:10 +0100

    Started MonthSorterNested listing months

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -9,6 +9,22 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    enum Month {
+        JANUARY(31), FEBRUARY(28), MARCH(31), APRIL(30),
+        MAY(31), JUNE(30), JULY(31), AUGUST(31),
+        SEPTEMBER(30), OCTOBER(31), NOVEMBER(30), DECEMBER(31);
+
+        private final int days;
+
+        private Month(int days) {
+            this.days = days;
+        }
+
+        public Month fromString(String string) {
+            return Month.this;
+        }
+    }
+
     @Override
     public Comparator<String> sortByDays() {
         return null;
