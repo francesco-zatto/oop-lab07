30bf7dd69929dfb7984df9db25fefcc1ef220557
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2022-11-09 20:40:39 +0100

    Completed first part (Multiple views)

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -4,6 +4,7 @@ import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
 import it.unibo.mvc.view.DrawNumberSwingView;
+import it.unibo.mvc.view.DrawNumberTerminalView;
 
 /**
  * Application entry-point.
@@ -27,5 +28,7 @@ public final class LaunchApp {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
         app.addView(new DrawNumberSwingView());
+        app.addView(new DrawNumberSwingView());
+        app.addView(new DrawNumberTerminalView());
     }
 }
